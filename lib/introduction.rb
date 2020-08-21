79c126a9eeac138a9be5929d21baf93bb8bbb085
Oscar A Castro <oacastro2@gmail.com>
2020-08-21 20:46:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ end
 introduction
 
 def introduction_with_language
-  puts "Hi, my name is #{name} and I am learning to program in #{language}." 
+  puts "Hi, my name is #'{name}' and I am learning to program in #'{language}'." 
 end   
 introduction_with_language
 
